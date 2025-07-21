5e5f6e57973b936848c41b2bde2a3540d28a99a5
devdavi02 <drmf135@gmail.com>
2025-07-21 13:07:31 -0300

    arquivo main

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI
-
+from app.controllers import usuarios_controller
+from app.database.db import Base, engine
 
 import uvicorn
 
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
-@app.get("/")
+app.include_router(usuarios_controller.router)
 
+@app.get("/")
 def read_root():
 
     return {"Hello": "World"}
-
-
 
 def main():
 
